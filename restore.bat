9aa85ce01a721a063c50d9b56544a60563e449e9
madchan <madchan912@gmail.com>
2025-02-19 04:00:00 +0900

    컨테이너에 폴더 생성하게 추가

--- restore.bat
+++ restore.bat
@@ -1,5 +1,13 @@
 @echo off
 echo [🔄] MongoDB 데이터 복원 중...
+
+:: 컨테이너 내부에 /restore 폴더 생성
+docker exec -it yonudoo-mongo mkdir -p /restore/YoNuDoo
+
+:: 백업 데이터 복사
 docker cp mongo_backup/YoNuDoo yonudoo-mongo:/restore/YoNuDoo
+
+:: 데이터 복원
 docker exec -it yonudoo-mongo mongorestore --username root --password 1234 --authenticationDatabase admin --dir /restore/YoNuDoo
+
 echo [✅] 데이터 복원 완료!
