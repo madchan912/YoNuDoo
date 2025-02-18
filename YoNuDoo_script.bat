2cf8d019ea7be3646e9ad5a093c85a035d96344a
madchan <madchan912@gmail.com>
2025-02-19 03:11:13 +0900

    배치 수정

--- YoNuDoo_script.bat
+++ YoNuDoo_script.bat
@@ -20,22 +20,44 @@ call :append_file_content "%TARGET_DIR%\main.py"
 call :append_file_content "%TARGET_DIR%\requirements.txt"
 call :append_file_content "%TARGET_DIR%\docker-compose.yml"
 call :append_file_content "%TARGET_DIR%\Dockerfile"
+call :append_file_content "%TARGET_DIR%\.env"
 
-:: .py 파일들 중 __init__.py 제외하고 venv 폴더 제외하며 소스 내용을 output 파일에 저장
+:: 모든 .py 파일들 중 __init__.py 제외하고 .venv와 .git 폴더 제외하며 소스 내용을 output 파일에 저장
 for /r "%TARGET_DIR%" %%f in (*.py) do (
+    set "filepath=%%f"
     if /i "%%~nxf" neq "__init__.py" (
+        echo !filepath! | findstr /i /c:".venv" /c:"\.git" > nul
+        if !errorlevel! neq 0 (
+            call :append_file_content "%%f"
+        )
+    )
+)
+
+:: 모든 .css 파일들 소스 내용 추가, .venv, .git 폴더 제외
+for /r "%TARGET_DIR%" %%f in (*.css) do (
+    set "filepath=%%f"
+    echo !filepath! | findstr /i /c:".venv" /c:"\.git" > nul
+    if !errorlevel! neq 0 (
         call :append_file_content "%%f"
     )
 )
 
-:: static 폴더 내의 css, js 파일들 소스 내용 추가, venv 제외
-for /r "%TARGET_DIR%\static" %%f in (*.css *.js) do (
-    call :append_file_content "%%f"
+:: 모든 .html 파일들 소스 내용 추가, .venv, .git 폴더 제외
+for /r "%TARGET_DIR%" %%f in (*.html) do (
+    set "filepath=%%f"
+    echo !filepath! | findstr /i /c:".venv" /c:"\.git" > nul
+    if !errorlevel! neq 0 (
+        call :append_file_content "%%f"
+    )
 )
 
-:: templates 폴더 내의 html 파일들 소스 내용 추가, venv 제외
-for /r "%TARGET_DIR%\templates" %%f in (*.html) do (
-    call :append_file_content "%%f"
+:: 모든 .js 파일들 소스 내용 추가, .venv, .git 폴더 제외
+for /r "%TARGET_DIR%" %%f in (*.js) do (
+    set "filepath=%%f"
+    echo !filepath! | findstr /i /c:".venv" /c:"\.git" > nul
+    if !errorlevel! neq 0 (
+        call :append_file_content "%%f"
+    )
 )
 
 echo 작업이 완료되었습니다.
@@ -43,8 +65,10 @@ goto :EOF
 
 :: 파일 내용을 output 파일에 추가하는 함수
 :append_file_content
-echo. >> "%OUTPUT_FILE%"
-echo === [FILE]: %1 === >> "%OUTPUT_FILE%"
-type "%1" >> "%OUTPUT_FILE%"
-echo. >> "%OUTPUT_FILE%"
+if exist "%~1" (
+    echo. >> "%OUTPUT_FILE%"
+    echo === [FILE]: %1 === >> "%OUTPUT_FILE%"
+    type "%1" >> "%OUTPUT_FILE%"
+    echo. >> "%OUTPUT_FILE%"
+)
 goto :EOF
